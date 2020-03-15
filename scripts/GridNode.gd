0581dddd2e4889f461f0a75376ee34210cc50a19
tehAndrew <andpa_kontakt@live.se>
2020-03-15 16:06:59 +0100

    Grid objects are now placed in the grid node tree

--- scripts/GridNode.gd
+++ scripts/GridNode.gd
@@ -11,8 +11,9 @@ var _covered : bool
 var _grid_object_arr : Array
 
 # Constructor.
-func _init(grid_object_arr : Array = []):
-	_grid_object_arr = grid_object_arr
+func _init(pos : Vector2 = Vector2(0, 0)):
+	_grid_object_arr = []
+	set_position(pos)
 	_covered = true
 
 # Uncover node, all contained objects will become visible. There is no need to cover the node 
@@ -29,16 +30,19 @@ func place_object(grid_object : GridObject) -> void:
 	else:
 		grid_object.uncover()
 	
-	grid_object.set_position(self.position)
+	add_child(grid_object)
 	_grid_object_arr.append(grid_object)
 
 # Remove the object from the node. The object should either be removed or placed in another node.
+# The object DOES NOT get removed.
 func take_object(grid_object : GridObject) -> GridObject:
 	var found_object : GridObject = null
 	var index : int = _grid_object_arr.find(grid_object)
 	
 	if (index != -1):
 		found_object = _grid_object_arr[index]
+		_grid_object_arr.remove(index)
+		remove_child(found_object)
 	
 	return found_object
 
@@ -48,5 +52,5 @@ func get_object_types() -> Array:
 	var types : Array = []
 	for grid_object in _grid_object_arr:
 		types.append(grid_object.get_type())
-		
+	
 	return types
