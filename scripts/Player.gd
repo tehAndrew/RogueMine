d52a570542b571e69289e6ccb6c74df638419224
tehAndrew <andpa_kontakt@live.se>
2020-03-17 17:15:42 +0100

    player movement updated

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,11 +5,14 @@ class_name Player
 signal request_movement(direction, obj_id)
 
 func _process(delta):
-	if (Input.is_action_pressed("ui_right")):
+	if (Input.is_action_just_pressed("ui_right")):
 		emit_signal("request_movement", "right", self)
-	elif (Input.is_action_pressed("ui_left")):
+		
+	elif (Input.is_action_just_pressed("ui_left")):
 		emit_signal("request_movement", "left", self)
-	elif (Input.is_action_pressed("ui_down")):
+			
+	elif (Input.is_action_just_pressed("ui_down")):
 		emit_signal("request_movement", "down", self)
-	elif (Input.is_action_pressed("ui_up")):
+			
+	elif (Input.is_action_just_pressed("ui_up")):
 		emit_signal("request_movement", "up", self)
