c6204963a242441abb2fec44568d8493048174c0
tehAndrew <andpa_kontakt@live.se>
2020-03-24 21:27:02 +0100

    changed random generation

--- scripts/GridGenerator.gd
+++ scripts/GridGenerator.gd
@@ -1,40 +1,71 @@
+# class GridGenerator ---
+# The grid is grouped by 4x4 partitions. The grid must therefore have a width and a height
+# divisible by 4. The mines is distributed throughout the partitions that does not contain
+# the player or the goal.
 extends Node2D
 
 const Grid = preload("res://scenes/Grid.tscn")
 
+const PATCH_SIZE : Vector2 = Vector2(4, 4)
+
+var _patch_amount : Vector2
 var _grid_size : Vector2
-var _mine_count : int
+var _mine_density : int
+var _mine_density_offset : int
 
 func _ready() -> void:
 	randomize()
 	generate()
 
 func generate() -> void:
-	_grid_size = Vector2(16, 16)
-	_mine_count = 20
+	_patch_amount = Vector2(4, 4)
+	_grid_size = _patch_amount * PATCH_SIZE
+	_mine_density = 2
+	_mine_density_offset = 1
+	
+	var patches : Array = []
+	
+	for x in range(0, _patch_amount.x):
+		for y in range(0, _patch_amount.y):
+			if (Vector2(x, y) != Vector2(0, 0) && Vector2(x, y) != Vector2(_patch_amount.x - 1, _patch_amount.y - 1)):
+				patches.append(generate_mine_patch(Vector2(x, y)))
 	
 	var grid : Node2D = Grid.instance()
 	grid.init(Vector2(0, 0), Vector2(512, 512), _grid_size)
+	add_child(grid)
 	
-	var rand_pos_arr : Array = []
-	for i in range(0, _mine_count):
+	for patch in patches:
+		print(patch.pos)
+		for mine_local_pos in patch.spawn_pos_arr:
+			grid.spawn_mine(patch.pos * PATCH_SIZE + mine_local_pos)
+	grid.spawn_player(Vector2(1, 1))
+
+func generate_mine_patch(var pos : Vector2) -> Patch:
+	var patch : Patch = Patch.new()
+	patch.type = "mine"
+	patch.pos = pos
+	patch.spawn_pos_arr = []
+	
+	var mine_amount : int = _mine_density + int(rand_range(-_mine_density_offset, _mine_density_offset + 1))
+	
+	for i in range(0, mine_amount):
 		var found_spot : bool = false
 		while (!found_spot):
-			var rand_x : int = 1 + randi() % int(_grid_size.x - 1)
-			var rand_y : int = 1 + randi() % int(_grid_size.y - 1)
+			var rand_x : int = randi() % int(PATCH_SIZE.x - 1)
+			var rand_y : int = randi() % int(PATCH_SIZE.y - 1)
 			var rand_pos : Vector2 = Vector2(rand_x, rand_y)
-			if (!rand_pos_arr.has(rand_pos)):
-				rand_pos_arr.append(rand_pos)
+			if (!patch.spawn_pos_arr.has(rand_pos)):
+				patch.spawn_pos_arr.append(rand_pos)
 				found_spot = true
 	
-	for rand_pos in rand_pos_arr:
-		grid.spawn_mine(rand_pos)
-	
-	grid.spawn_player(Vector2(0, 0))
-	
-	add_child(grid)
+	return patch
 
 func _process(var delta) -> void:
 	if (Input.is_action_just_pressed("ui_select")):
 		remove_child(get_child(0))
 		generate()
+
+class Patch:
+	var type : String
+	var pos : Vector2
+	var spawn_pos_arr : Array
